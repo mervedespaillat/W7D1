ba8b4d4a3f87fef3ffea7f8695bd98d75aeba89c
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-04-17 14:47:59 -0400

    working on user model

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,47 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.now(self.password_digest)
+
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+    
+        user = User.find_by(username: username)
+            if user && user.is_password?(password)
+                user
+            else
+                nil
+            end
+    end
+
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64(16)
+            return session_token unless User.exists?(session_token: session_token)
+          end
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+end
