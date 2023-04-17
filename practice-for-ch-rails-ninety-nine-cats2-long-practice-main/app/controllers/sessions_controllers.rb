747a7dbd770fe4ce763e1caaf47e1bd25e2117da
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-04-17 15:17:09 -0400

    add controllers

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controllers.rb
@@ -0,0 +1,29 @@
+class SessionsControllers < ApplicationController
+    def new
+        @user = User.new
+        render :new
+    end 
+
+    def create
+        username = params[:user][:username]
+        password = params[:user][:password]
+
+        @user = User.find_by_credentials(username,password)
+
+        if @user.save 
+            login!(@user)
+            redirect_to cats_url
+        else
+            user = User.new(username: username)
+            render :new 
+        end
+
+    end
+
+    def destroy
+        logout!
+
+        redirect_to new_session_url
+    end
+
+end
